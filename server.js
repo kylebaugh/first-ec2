09ddf2e68424a420150c1ab63ebc1c23188bee98
kylebaugh <kylebaugh1@gmail.com>
2022-11-21 14:04:17 -0700

    adjusted app.use

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/index.html`))
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
-  });
 
 app.listen(4000,
    () => console.log(`server running on 4000`)
