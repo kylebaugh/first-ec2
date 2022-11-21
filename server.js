dd46eec95fdde4c94c13c8dcb6eafaa24f293d28
kylebaugh <kylebaugh1@gmail.com>
2022-11-21 14:03:29 -0700

    added filepath to server

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express()
 
 app.use(express.static(`${__dirname}/public`))
 
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+
 app.listen(4000,
    () => console.log(`server running on 4000`)
 )
