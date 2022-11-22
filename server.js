8fae8052eb412a66d99d52ca51499a32e6fd9ca5
kylebaugh <kylebaugh1@gmail.com>
2022-11-22 14:54:19 -0700

    added rollbar

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 app.use(express.static(`${__dirname}/public`))
+app.use(express.json())
+app.use(cors())
+
+const Rollbar = require('rollbar')
+
+const rollbar = new Rollbar({
+   accessToken: 'ed4ff7e596204629ac0441be23807482',
+   captureUncaught: true,
+   captureUnhandledRejections: true
+})
+
+rollbar.log('Hello world!')
 
 
 app.listen(4000,
